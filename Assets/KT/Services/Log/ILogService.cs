19d4fc245e42258812006d07d27dae6a9f7bc7a0
kevin-game <15205925919@163.com>
2025-01-22 22:48:39 +0800

    [add]log service

---
+++ Assets/KT/Services/Log/ILogService.cs
@@ -0,0 +1,9 @@
+﻿namespace KT
+{
+    public interface ILogService
+    {
+        public void Info(string message);
+        public void Warning(string message);
+        public void Error(string message);
+    }
+}
---
+++ Assets/KT/Services/Log/LogService.cs
@@ -0,0 +1,20 @@
+﻿namespace KT
+{
+    public class LogService : ILogService
+    {
+        public void Info(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+        
+        public void Warning(string message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+        
+        public void Error(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+}
